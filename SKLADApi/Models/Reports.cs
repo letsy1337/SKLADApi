04aa681675939cb329109fc2ac1357f4be182b67
tsukanovd <letsy1337@gmail.com>
2024-05-24 03:21:21 +0300

    Змінив тип змінної в класі Reports з byte[] на string

--- SKLADApi/Models/Reports.cs
+++ SKLADApi/Models/Reports.cs
@@ -5,7 +5,7 @@
         public Guid Id { get; set; }
         public Guid Report_type { get; set; }
         public DateTime Creation_date { get; set; }
-        public Byte[]? File { get; set; }
+        public string File { get; set; }
         public Guid Created_by { get; set; }
     }
 }
